0ccaac40ef221a0f1feebe1d8afdbf5325d0630f
ZhouyangXie <xiezhouyang@unionbigdata.com>
2021-07-15 12:19:27 +0800

    fix the mis-implementation of ranking

--- pseudopruner/channel_pruner/comp_aware_pruner/ranking.py
+++ pseudopruner/channel_pruner/comp_aware_pruner/ranking.py
@@ -1,160 +1,143 @@
-import torch
+import numpy as np
+from scipy.linalg import block_diag
+from scipy.linalg.lapack import dtrtri
 
 
-def rank_channels(module, device):
+def rank_channels(module):
     weight = module.weight.clone()
     if hasattr(module, 'prune_weight_mask'):
         weight[module.prune_weight_mask, ] = 0
 
-    if isinstance(module, torch.nn.Conv2d):
-        return _rank_channels_conv(weight, module.mu, module.sigma, device)
-    elif isinstance(module, torch.nn.Linear):
-        return _rank_channels_linear(weight, module.mu, module.sigma, device)
+    return _rank_channels_linear_conv(weight, module.mu, module.sigma)
+
+
+def _low_tri_inv(L):
+    L_inv, info = dtrtri(L, lower=1)
+    if info == 0:
+        return L_inv
     else:
-        raise NotImplementedError
+        raise RuntimeError(f'lapack.dtrtri: error code {info}')
 
 
-def _rank_channels_conv(weight, mu, sigma, device):
-    sigma_cc = sigma - torch.outer(mu, mu)
-    sigma_cc = sigma_cc.to(
-        dtype=torch.float32, device=device)
+def _torch2numpy(tensor):
+    return tensor.detach().cpu().numpy().astype(np.float64)
 
+
+def _rank_channels_linear_conv(weight, mu, sigma):
+    weight = _torch2numpy(weight)
+    if weight.ndim == 2:
+        weight = weight.reshape(weight.shape + (1, 1))
+    mu = _torch2numpy(mu)
+    sigma = _torch2numpy(sigma)
+
+    sigma_cc = sigma - np.outer(mu, mu)
     # set zero-var channels to a minimal positive variance
-    for i in range(len(sigma_cc)):
-        if sigma_cc[i, i] == 0:
-            sigma_cc[i, i] += 1e-20
+    for i, sigma_ii in enumerate(sigma_cc.diagonal()):
+        if sigma_ii <= 0:
+            sigma_cc[i, i] = 1e-20
 
-    N, out_channels, kw, kh = weight.shape
+    N, C, kw, kh = weight.shape
     dim = len(mu)
-    assert dim == out_channels * kw * kh
+    assert dim == C * kw * kh
+    ks = kw * kh
 
-    weight = weight.to(device=device, dtype=torch.float32)
-    weight = weight.reshape((N, -1))  # NxDim
-    D = weight.matmul(sigma_cc.to(device))
+    weight = weight.reshape((N, dim))  # NxDim
+    w_sigma_cc = weight.dot(sigma_cc)
 
-    selected = torch.zeros(dim, dtype=bool, device=device)
-    all_inds = torch.arange(dim)
-    S = []
-    L_s_inv = None
+    ranks = []
+    # L_s_inv = None
+    sigma_ss_inv = None
 
-    for i in range(dim):
-        torch.cuda.empty_cache()
+    for i in range(C):
         # if the first round
-        if L_s_inv is None:
-            sigma_all_t = sigma_cc.diag()
-            D_all_t = torch.square(D).sum(axis=0)
-            scores = D_all_t/sigma_all_t
-            max_j = int(torch.argmax(scores))
+        if sigma_ss_inv is None:
+            max_score = -1
+            max_ind = None
+            max_sigma_ss_inv = None
+            for j in range(C):
+                w_sigma_ct = w_sigma_cc[:, (j*ks):((j+1)*ks)]
+                sigma_tt = sigma_cc[(j*ks):((j+1)*ks), (j*ks):((j+1)*ks)]
 
-            selected[max_j] = True
-            S.append(max_j)
-            L_s_inv = torch.ones((1, 1), device=device, dtype=torch.float32)
-            L_s_inv[:] = 1/sigma_all_t[max_j].sqrt()
+                try:
+                    L_t = np.linalg.cholesky(sigma_tt)
+                    L_t_inv = _low_tri_inv(L_t)
+                except (np.linalg.LinAlgError, RuntimeError):
+                    # numerically unstable
+                    continue
+
+                score = w_sigma_ct.dot(L_t_inv.T)
+                score = np.square(score).sum()
+
+                if score > max_score:
+                    max_score = score
+                    max_ind = j
+                    max_sigma_ss_inv = L_t_inv.T.dot(L_t_inv)
+
+            sigma_ss_inv = max_sigma_ss_inv
+            assert sigma_ss_inv.shape == (ks, ks)
+            ranks.append(max_ind)
 
         else:
-            T = ~selected
-            S_cuda = torch.tensor(S, device=device, dtype=torch.long)
+            non_rank = [
+                r for r in range(C) if r not in ranks
+            ]
+            s_inds = np.concatenate([
+                np.arange(ks*selected_i, ks*(selected_i + 1))
+                for selected_i in ranks
+            ])  # in selection order
+            p_inds = np.concatenate([
+                np.arange(ks*unselected, ks*(unselected + 1))
+                for unselected in non_rank
+            ])  # incremental
 
-            sigma_tt_diag = sigma_cc.diag()[T]
-            sigma_st = sigma_cc[S_cuda][:, T]
+            sigma_sc = sigma_cc[s_inds, :]
 
-            D_s = D[:, S_cuda]  # N X S
-            D_t = D[:, T]  # N X T
+            sigma_sp = sigma_sc[:, p_inds]
+            SssinvSsp = sigma_ss_inv.dot(sigma_sp)
+            SpsSssinvSsp = sigma_sp.T.dot(SssinvSsp)
 
-            b = L_s_inv.matmul(sigma_st)  # S X T
-            bbt = (b * b).sum(axis=0)
+            w_sigma_cp = w_sigma_cc[:, p_inds]
+            w_sigma_cs = w_sigma_cc[:, s_inds]
+            score_base_np = w_sigma_cp - w_sigma_cs.dot(SssinvSsp)
 
-            r = torch.sqrt(sigma_tt_diag - bbt)  # T
-            rows = -(L_s_inv.T.matmul(b))/r  # S X T
+            max_score = -1
+            max_ind = None
+            max_j_ind = None
+            max_A = None
 
-            scores = D_s.matmul(rows) + D_t/r  # N X T
-            scores = torch.square(scores).sum(axis=0)  # T
+            for j_ind, j in enumerate(non_rank):
+                sigma_tt = sigma_cc[(j*ks):((j+1)*ks), (j*ks):((j+1)*ks)]
+                temp_tt = sigma_tt - SpsSssinvSsp[
+                    (j_ind*ks):((j_ind+1)*ks), (j_ind*ks):((j_ind+1)*ks)
+                ]
+                try:
+                    A_inv = np.linalg.cholesky(temp_tt)
+                    A = _low_tri_inv(A_inv)
+                except np.linalg.LinAlgError:
+                    # numerically unstable
+                    raise RuntimeWarning
+                    continue
 
-            max_j = torch.argmax(scores)
-            assert not torch.isnan(scores).any()
-            assert not torch.isinf(scores).any()
+                score = score_base_np[:, ks*j_ind:(ks*j_ind+ks)]
+                score = score.dot(A.T)
+                score = np.square(score).sum()
 
-            max_r = r[max_j].reshape((1,))
-            max_row = rows[:, max_j].flatten()
-            L_s_inv = torch.block_diag(L_s_inv, 1/max_r)
-            L_s_inv[-1, 0:-1] = max_row
+                if score > max_score:
+                    max_score = score
+                    max_ind = j
+                    max_j_ind = j_ind
+                    max_A = A.copy()
 
-            new_s = int(all_inds[T][max_j])
-            selected[new_s] = True
-            S.append(new_s)
+            SssinvSsmax = SssinvSsp[:, (max_j_ind*ks):((max_j_ind+1)*ks)]
+            ATA = max_A.T.dot(max_A)  # ks x ks
+            RTA = - SssinvSsmax.dot(ATA)  # S x ks
+            RTR = - RTA.dot(SssinvSsmax.T)  # S x ks
+            sigma_ss_inv = block_diag(
+                sigma_ss_inv + RTR, ATA
+            )
+            sigma_ss_inv[-ks:, 0:-ks] = RTA.T
+            sigma_ss_inv[0:-ks, -ks:] = RTA
 
-    return S
+            ranks.append(max_ind)
 
-
-def _rank_channels_linear(weight, mu, sigma, device):
-    sigma_cc = sigma - torch.outer(mu, mu)
-    sigma_cc = sigma_cc.to(
-        dtype=torch.float32, device=device)
-
-    # set zero-var channels to a minimal positive variance
-    for i in range(len(sigma_cc)):
-        if sigma_cc[i, i] == 0:
-            sigma_cc[i, i] += 1e-20
-
-    N, _dim = weight.shape
-    dim = len(mu)
-    assert dim == _dim
-    assert sigma_cc.shape == (dim, dim)
-
-    # N X dim
-    weight = weight.to(device=device, dtype=torch.float32)
-    # N X dim
-    D = weight.matmul(sigma_cc.to(device))
-
-    selected = torch.zeros(dim, dtype=bool, device=device)
-    all_inds = torch.arange(dim)
-    S = []
-    L_s_inv = None
-
-    for i in range(dim):
-        torch.cuda.empty_cache()
-        # if the first round
-        if L_s_inv is None:
-            sigma_all_t = sigma_cc.diag()
-            D_all_t = torch.square(D).sum(axis=0)
-            scores = D_all_t/sigma_all_t
-            max_j = int(torch.argmax(scores))
-
-            selected[max_j] = True
-            S.append(max_j)
-            L_s_inv = torch.ones((1, 1), device=device, dtype=torch.float32)
-            L_s_inv[:] = 1/sigma_all_t[max_j].sqrt()
-
-        else:
-            T = ~selected
-            S_cuda = torch.tensor(S, device=device, dtype=torch.long)
-
-            sigma_tt_diag = sigma_cc.diag()[T]
-            sigma_st = sigma_cc[S_cuda][:, T]
-
-            D_s = D[:, S_cuda]  # N X S
-            D_t = D[:, T]  # N X T
-
-            b = L_s_inv.matmul(sigma_st)  # S X T
-            bbt = (b * b).sum(axis=0)
-
-            r = torch.sqrt(sigma_tt_diag - bbt)  # T
-            rows = -(L_s_inv.T.matmul(b))/r  # S X T
-
-            scores = D_s.matmul(rows) + D_t/r  # N X T
-            scores = torch.square(scores).sum(axis=0)  # T
-
-            max_j = torch.argmax(scores)
-            assert not torch.isnan(scores).any()
-            assert not torch.isinf(scores).any()
-
-            max_r = r[max_j].reshape((1,))
-            max_row = rows[:, max_j].flatten()
-            L_s_inv = torch.block_diag(L_s_inv, 1/max_r)
-            L_s_inv[-1, 0:-1] = max_row
-
-            new_s = int(all_inds[T][max_j])
-            selected[new_s] = True
-            S.append(new_s)
-
-    return S
+    return ranks
